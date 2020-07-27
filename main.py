d255cf2027c533fc751012d4a86bc133ccf193c1
zitto <zhjc1124@gmail.com>
2020-07-27 16:27:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
-
-base_url = 'http://www.gwbn-vpn.top'
+requests.packages.urllib3.disable_warnings()
+base_url = 'http://www.****.top'
 
 
 def checkin():
@@ -11,21 +11,26 @@ def checkin():
     email = email[0] + '%40' + email[1]
 
     session = requests.session()
+    
+    session.get(base_url, verify=False)
+
     login_url = base_url + '/auth/login'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
     }
-    post_data = 'email=' + email + '&passwd=' + password + '&code=&remember_me=week'
+
+    post_data = 'email=' + email + '&passwd=' + password + '&code='
     post_data = post_data.encode()
     response = session.post(login_url, post_data, headers=headers, verify=False)
-
+    
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
         'Referer': base_url + '/user'
     }
 
-    response = session.post(base_url + '/user/checkin', headers=headers)
+    response = session.post(base_url + '/user/checkin', headers=headers, verify=False)
+    print(response.text)
 
 while True:
     try:
